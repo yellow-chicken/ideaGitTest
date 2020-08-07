05bf549227b4655ae77a3cb1fff55ff64bdfb99c
huangji <lsc_0926@163.com>
2020-08-07 14:04:31 +0800

    把idea工程添加到git

---
+++ git_test/src/HelloGit.java
@@ -0,0 +1,10 @@
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
